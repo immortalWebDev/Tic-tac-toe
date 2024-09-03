3342e549c7d5eaab5afe8d3db76d9e05d02abc49
immortalWebDev <pgbadgujar007@gmail.com>
2024-09-04 00:38:18 +0530

    Updated App

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,17 @@
 import React from "react";
 import Game from "./components/Game";
+import Footer from "./components/Footer/Footer.jsx";
+import Rules from "./components/Rules/Rules.jsx";
+import "./App.css";
 
 function App() {
   return (
     <>
-      <Game/>
+      <div className="app-container">
+        <Rules />
+        <Game />
+      </div>
+      <Footer />
     </>
   );
 }
